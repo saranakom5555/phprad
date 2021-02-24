fbe9a20261812a3829ef1ea204b019e56dd99ae7
saranakom <saranakom2328@gmail.com>
2021-02-24 14:49:07 +0700

    phprad

---
+++ helpers/Menu.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+	public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => '<i class="fa fa-apple "></i>'
+		),
+		
+		array(
+			'path' => 'category', 
+			'label' => 'Category', 
+			'icon' => '<i class="fa fa-align-justify "></i>'
+		),
+		
+		array(
+			'path' => 'post', 
+			'label' => 'Post', 
+			'icon' => '<i class="fa fa-android "></i>'
+		),
+		
+		array(
+			'path' => 'user', 
+			'label' => 'User', 
+			'icon' => '<i class="fa fa-ambulance "></i>'
+		)
+	);
+
+	
+	
+	public static $name = array();
+
+}
